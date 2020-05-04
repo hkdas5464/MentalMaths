0cb29f63c77f0d014a112abdaa923531eae3ef0b
hkdas5464 <hkdas5464@gmail.com>
2020-05-04 23:27:12 +0530

    Updates

---
+++ precache-manifest.36728af2fe15d1f6ef9c7ab2589e3163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3b19b2188fedd91f95f6b6cca3c81d",
+    "url": "/MentalMaths/index.html"
+  },
+  {
+    "revision": "a04f7219be894f6795b4",
+    "url": "/MentalMaths/static/css/main.5f6fc2ee.chunk.css"
+  },
+  {
+    "revision": "23b9be1b82ef3e7f6396",
+    "url": "/MentalMaths/static/js/2.3296985a.chunk.js"
+  },
+  {
+    "revision": "a04f7219be894f6795b4",
+    "url": "/MentalMaths/static/js/main.0f2d0c39.chunk.js"
+  },
+  {
+    "revision": "b92a5a274c39cc9e748b",
+    "url": "/MentalMaths/static/js/runtime~main.7c4a7bf0.js"
+  },
+  {
+    "revision": "00e7fb1da06c83af451a6207c2622bfa",
+    "url": "/MentalMaths/static/media/logo.00e7fb1d.png"
+  }
+]);
